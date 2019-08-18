445e70800c7801cd4e6facb6caee9a90e15cdb07
Gabriel Lopez <globant_gsosa@iseatz.com>
2019-08-18 12:11:56 -0500

    Ejetcicio con Async/await

--- gifSearch.js
+++ gifSearch.js
@@ -1,6 +1,6 @@
 
 const giphyAPI = `http://api.giphy.com/v1/gifs/search?api_key=Rb8SgnqXnK9jVtUTLHAdvjQkf3fjnj8O&q=`;
-const wordAPI =  `https://random-word-api.herokuapp.com/word?number=1&key=X3K1GBVD`;
+const wordAPI =  `https://random-word-api.herokuapp.com/word?number=1&key=1JT6LE1H`;
 
 
 function getGif(){
@@ -15,7 +15,7 @@ function getGif(){
             const { images: {fixed_height_small: url}  } = data
 
             const urlGet = _.get(json.data,'0.images.fixed_height_small.url');
-            º
+
             document.getElementById('myImage').src = urlGet;
         })
         .catch( err => {
@@ -24,7 +24,43 @@ function getGif(){
 
 }
 
-
 function transformJson( response ) {
     return response.json()
+}
+
+
+function obten3Imagenes(){
+
+    const promiseJar = [
+        asyncGetGif(),
+        asyncGetGif(),
+        asyncGetGif()
+    ]
+
+    Promise.all(promiseJar).then(
+        ([primera, segunda, tercera])=> {
+
+
+            const urlGetPrimera = _.get(primera.data,'0.images.fixed_height_small.url');
+            const urlGetSegunda = _.get(segunda.data,'0.images.fixed_height_small.url');
+            const urlGetTercera = _.get(tercera.data,'0.images.fixed_height_small.url');
+
+            document.getElementById('myImagePrimera').src = urlGetPrimera;
+            document.getElementById('myImageSegunda').src = urlGetSegunda;
+            document.getElementById('myImageTercera').src = urlGetTercera;
+
+
+        }
+    )
+
+}
+
+
+async function asyncGetGif(){
+
+    const wordAPIResult = await fetch(wordAPI)
+    const [word] = await wordAPIResult.json()
+
+    const  giphyResult = await fetch(giphyAPI + word);
+    return giphyResult.json();
 }
